fff14ae58c35d14edca4e5978c783111a1a8d92f
soyxni <weehee6947@gmail.com>
2025-09-24 23:01:19 +0900

    query 주석 추가(참고용)

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -8,9 +8,24 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Random;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
 
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
+
+//    select
+//        p.id,
+//        p.title as product_title,
+//        pf.product_id as product_id,
+//        pf.folder_id as folder_id
+//    from
+//            product p left join product_folder pf
+//                on p.id = pf.product_id
+//    where p.user_id = 1
+//        and
+//        pf.folder_id = 3
+//    order by p.id
+//        limit 10, 20;
 }
